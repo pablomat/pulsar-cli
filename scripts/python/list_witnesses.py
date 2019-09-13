75763f01b171b780c40b5e051809431f2d80b5f5
Pablo Staiano <1355981+pablomat@users.noreply.github.com>
2019-09-13 15:40:12 +0200

    Update steem instance

--- scripts/python/list_witnesses.py
+++ scripts/python/list_witnesses.py
@@ -3,15 +3,7 @@ from beem.witness import ListWitnesses
 from beem.instance import set_shared_steem_instance
 
 from beem import Steem
-stm = Steem(node=["https://apidev.blkcc.xyz"], custom_chains={"PULSAR":
-    {'chain_assets': [{'asset': '@@000000013', 'id': 0, 'precision': 3, 'symbol': 'EUR'},
-                      {'asset': '@@000000021', 'id': 1, 'precision': 3, 'symbol': 'PULSE'},
-                      {'asset': '@@000000037', 'id': 2, 'precision': 6, 'symbol': 'VESTS'}],
-     'chain_id': '07c687c01f134adaf217a9b9367d1cef679c3c020167fdd25ee8c403f687528e',
-     'min_version': '0.101.0',
-     'prefix': 'EUR'}
-    }
-)
+stm = Steem(node=["https://apidev.blkcc.xyz"])
 
 set_shared_steem_instance(stm)
 
